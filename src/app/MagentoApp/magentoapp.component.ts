745bf9f1b86233a960c6ba4541b97396706e2d9f
Nir Goldman <refaelgold@gmail.com>
2016-06-12 09:26:15 +0300

    get all category from M2 website

--- src/app/MagentoApp/magentoapp.component.ts
+++ src/app/MagentoApp/magentoapp.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Example} from './services/example';
+import {Magento2} from './services/magento2';
 
 /*
  * We're loading this component asynchronously
@@ -13,47 +14,59 @@ console.log('`Magento App` component loaded asynchronously');
 @Component({
   selector: 'magentoapp',
   providers: [
-    Example
+    Example,
+    Magento2
   ],
   styles: [`
   `],
   template: `
     <div>
-        <h1>MagentoApp</h1>
-        <button (click)="onTestGet()">test GET request</button>
-        <p>output:{{getData}}</p>
-        <button (click)="onTestPost()">test POST request</button>
-        <p>output:{{postData}}</p>
+        <!--<h1>MagentoApp</h1>-->
+        <!--<button (click)="onTestGet()">test GET request</button>-->
+        <!--<p>output:{{getData}}</p>-->
+        <!--<button (click)="onTestPost()">test POST request</button>-->
+        <!--<p>output:{{postData}}</p>-->
+
+        <button (click)="getAllM2Products()">Get All Magento Prodcuts</button>
+        <p>output:{{getAllM2ProductsData}}</p>
 
     </div>
   `
 })
 export class MagentoApp {
 
-  getData:string;
-  postData:string;
+  // getData:string;
+  // postData:string;
+  getAllM2ProductsData:string;
 
 
-  constructor(private _HttpService:Example) {
-
-
-  }
+  constructor(private _HttpService:Magento2 ) {}
 
 
 
-  onTestGet(){
-    this._HttpService.getCurrentTime()
+  // onTestGet(){
+  //   this._HttpService.getCurrentTime()
+  //       .subscribe(
+  //           data=>this.getData=JSON.stringify(data),
+  //           error=>alert(error),
+  //           ()=>console.log("finished")
+  //       )
+  // }
+  //
+  // onTestPost(){
+  //   this._HttpService.postJson()
+  //       .subscribe(
+  //           data=>this.postData=JSON.stringify(data),
+  //           error=>alert(error),
+  //           ()=>console.log("finished")
+  //       )
+  // }
+
+
+  getAllM2Products(){
+    this._HttpService.getAllM2Products()
         .subscribe(
-            data=>this.getData=JSON.stringify(data),
-            error=>alert(error),
-            ()=>console.log("finished")
-        )
-  }
-
-  onTestPost(){
-    this._HttpService.postJson()
-        .subscribe(
-            data=>this.postData=JSON.stringify(data),
+            data=>this.getAllM2ProductsData=JSON.stringify(data),
             error=>alert(error),
             ()=>console.log("finished")
         )
---
+++ src/app/MagentoApp/services/magento2.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by Nir Goldman on 12/06/2016.
+ */
+import {Injectable} from 'angular2/core';
+import {Http,Headers} from 'angular2/http';
+
+@Injectable()
+export class Magento2 {
+    constructor(private _http: Http) {
+
+    }
+
+    getAllM2Products(){
+        var headers=new Headers();
+        headers.append('Content-Type','application/x-www-form-urlencoded');
+
+        return this._http.get('http://www.mageplayground.tk/rest/V1/categories',{
+            headers:headers
+        })
+            .map(res=>res.json())
+    }
+}
