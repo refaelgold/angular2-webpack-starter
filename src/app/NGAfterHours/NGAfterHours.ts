09b57ee9d8c3a6916e57ef7f9aa2d660e50fed98
Nir Goldman <refaelgold@gmail.com>
2016-06-26 18:50:49 +0300

    new module for Angular js-IL after hours

---
+++ src/app/NGAfterHours/NGAfterHours.ts
@@ -0,0 +1,47 @@
+import {Component} from 'angular2/core';
+
+/*
+ * We're loading this component asynchronously
+ * We are using some magic with es6-promise-loader that will wrap the module with a Promise
+ * see https://github.com/gdi2290/es6-promise-loader for more info
+ */
+
+console.log('`NG after hours` component loaded asynchronously');
+
+@Component({
+    selector: 'NGAfterHours',
+    styles: [ require('./ngafterhours.css') ],
+    template: require('./ngafterhours.html')
+})
+
+
+
+export class NGAfterHours {
+    constructor() {
+
+    }
+
+    ngOnInit() {
+        console.log('hello `ng after hours` component');
+        // static data that is bundled
+        // var mockData = require('assets/mock-data/mock-data.json');
+        // console.log('mockData', mockData);
+        // if you're working with mock data you can also use http.get('assets/mock-data/mock-data.json')
+        // this.asyncDataWithWebpack();
+    }
+    asyncDataWithWebpack() {
+        // you can also async load mock data with 'es6-promise-loader'
+        // you would do this if you don't want the mock-data bundled
+        // remember that 'es6-promise-loader' is a promise
+        // var asyncMockDataPromiseFactory = require('es6-promise!assets/mock-data/mock-data.json');
+        // setTimeout(() => {
+        //
+        //   let asyncDataPromise = asyncMockDataPromiseFactory();
+        //   asyncDataPromise.then(json => {
+        //     console.log('async mockData', json);
+        //   });
+        //
+        // });
+    }
+
+}
